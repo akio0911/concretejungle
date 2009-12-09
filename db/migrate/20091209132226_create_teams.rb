9074c0b655f808cc2fe1eb4544a1033780db70d4
akio0911 <akio0911@gmail.com>
2009-12-09 23:27:43 +0900

    user_idを削除して、team_idを追加

--- db/migrate/20091209132226_create_teams.rb
+++ db/migrate/20091209132226_create_teams.rb
@@ -2,13 +2,14 @@ class CreateTeams < ActiveRecord::Migration
   def self.up
     create_table :teams do |t|
       t.string :name
-      t.integer :user_id
 
       t.timestamps
     end
+    add_column :users, :team_id, :integer
   end
 
   def self.down
+    remove_column :users, :team_id
     drop_table :teams
   end
 end
